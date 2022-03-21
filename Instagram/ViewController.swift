4f2136aa9d6f1b229f5a8b044bb0adb83fa481c8
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-21 15:05:59 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Chelsea Chu on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
